de4bb1669048028d0253ee82fdeca3409eaa836e
Juanillo7830 <jvm0008@alu.medac.es>
2025-05-02 13:17:46 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -26,7 +26,7 @@ public class Main {
         Scanner teclado = new Scanner(System.in);
         double a=0;
         double b=0;
-        double z=3;
+        double z=5;
         Calculadora calculadora1 = new Calculadora(a, b);
         boolean fin = true;  
        
